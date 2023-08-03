262dc1495da9514a750d65599ba829d35ad10734
aurahl <141286970+aurahl@users.noreply.github.com>
2023-08-03 08:13:38 +0700

    Add files via upload

---
+++ Hanan Aura 5.cpp
@@ -0,0 +1,36 @@
+#include <cstdlib>
+#include <iostream>
+using namespace std;
+
+int Permutasi (int x, int y) {
+	int Nilai_Permut, selisih;
+	int faktorial = 1;
+	selisih = x - y;
+	
+	while (x > 0) {
+	faktorial = faktorial * x;
+	x = x - 1;
+	}
+		Nilai_Permut = faktorial/selisih;
+		return(Nilai_Permut);
+}
+
+int main () {
+	int n, r;
+	cout << "Masukan Nilai n: ";
+	cin >> n;
+	while (n < 0) {
+	cout << "Masukan Nilai N Kembali: ";
+	cin >> n;
+	}
+	cout << "Masukan Nilai r: ";	
+	cin >> r;
+	while (r < 0) {
+	cout << "Masukan Nilai r Kembali: ";
+	cin >> r;
+}
+	cout << "Nilai Permutasi " << r << " dari " << n << " adalah " << Permutasi (n, r) << endl;
+	
+	system ("PAUSE");
+	return(0);
+}
